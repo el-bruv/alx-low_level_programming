d812651166a74bcf146278245e04c27f8be188b2
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-08 16:35:51 -0700

    add script to print sign of random input number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,15 @@
-if (n > 0)
-{
-	printf("%i is positive", n);
-}
-else
-{
-	if (n < 0)
+	if (n > 0)
 	{
-		printf("%i is negative", n);
+		printf("%i is positive\n", n);
 	}
 	else
 	{
-		printf("%i is zero", n);
+		if (n < 0)
+		{
+			printf("%i is negative\n", n);
+		}
+		else
+		{
+			printf("%i is zero\n", n);
+		}
 	}
-}
