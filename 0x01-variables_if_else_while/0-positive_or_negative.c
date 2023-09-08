da5b905d7fcd0dc63421e255b101b4d681a491b1
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-08 16:23:21 -0700

    add script to print sign of random inout number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,7 @@
 if (n > 0)
-{
-	printf("%i is positive", n)
-}
-if (n < 0)
-{
-	printf("%i is negative", n)
-}
+	printf("%i is positive", n);
 else
-{
-	printf("%i is zero", n)
-}
+	if (n < 0)
+		printf("%i is negative", n);
+	else
+		printf("%i is zero", n);
