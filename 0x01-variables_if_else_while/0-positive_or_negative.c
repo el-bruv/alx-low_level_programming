5cd4065a93f5f4b114cd31168225817617f685c7
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-08 19:10:49 -0700

    change conditional type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,16 +6,15 @@
  * Return: display positive, negative or zero
  */
 
-if (n > 0)
-{
-	printf("%i is positive\n", n);
-}
-else if (n < 0)
-{
-	printf("%i is negative\n", n);
-}
-else
-{
-	printf("%i is zero\n", n);
-}
-
+	if (n > 0)
+	{
+		printf("%i is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%i is negative\n", n);
+	}
+	else
+	{
+		printf("%i is zero\n", n);
+	}
