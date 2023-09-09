01f922942a877a8a580245039f4e957611f6e3a3
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-08 17:13:07 -0700

    add script to selectively print alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - selective character printing
+ *
+ * Description: print alphabet in lowercase except q and e
+ *
+ * Return: zero if success
+ */
+
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a <= 'z')
+	{
+		if (a != 'q' || a != 'e')
+			putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
