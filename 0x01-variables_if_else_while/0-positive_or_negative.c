b1be61d4e2de7640b7c6b5cd5689e4e3f217b383
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-08 18:52:13 -0700

    change conditional type

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,15 +1,21 @@
+/**
+ * custom - add to existing source code
+ *
+ * Description: determine and print sign of number
+ *
+ * Return: display positive, negative or zero
+ */
+
 if (n > 0)
 {
 	printf("%i is positive\n", n);
 }
+else if (n < 0)
+{
+	printf("%i is negative\n", n);
+}
 else
 {
-	if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	else
-	{
-		printf("%i is zero\n", n);
-	}
+	printf("%i is zero\n", n);
 }
+
