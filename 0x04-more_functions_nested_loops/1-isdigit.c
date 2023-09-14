3b00198cc738360c0842a555f8057a21dae62bd2
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-14 16:15:18 -0700

    add isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
  * _isdigit - function that checks if character is a digit
+ *@c: character to check if is digit
  *
  * Return: one if is digit, zero if is not digit
  */
+
 int _isdigit(int c)
 {
-	if (c => 0 && c <= 9)
+	if ((c => 0) && (c <= 9))
 		return (1);
 	return (0);
 }
