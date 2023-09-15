a6451d33d112a250cd57ff884d18d29834e81406
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-14 19:12:57 -0700

    add c function block

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - print out decimal digits
+ *
+ * Return: void, so nothing
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2)
+			if (i != 4)
+				_putchar(48 + i);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,5 +11,6 @@ void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
+int _putchar(char c);
 
 #endif
