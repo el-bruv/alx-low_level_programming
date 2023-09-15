3f3d664069416dca5daea40a624c545c3182ad11
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-14 17:38:02 -0700

    add another function

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - print out decimal digits
+ *
+ * Return: void, so nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i = 0; i < 10; i++)
+		_putchar(i);
+	_putchar('\n');
+	return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * main.h - header file containing prototypes
  *
  */
+void print_numbers(void);
 int mul(int a, int b);
 int _isdigit(int c);
 int _isupper(int c);
