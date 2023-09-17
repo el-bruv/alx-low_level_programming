175f95967f27866d0e566f3ff75ccc4b22c943c9
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-17 09:55:01 -0700

    add rest of program body to function

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 /**
- * custom - add to existing source code
+ * main - add to existing source code
  *
  * Description: determine and print sign of number
  *
- * Return: display positive, negative or zero
+ * Return: zero if success
  */
+int main(void)
+{
+	int n;
 
-if(n > 0)
-	printf("%i is positive\n", n);
-else if(n < 0)
-	printf("%i is negative\n", n);
-else
-	printf("%i is zero\n", n);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
