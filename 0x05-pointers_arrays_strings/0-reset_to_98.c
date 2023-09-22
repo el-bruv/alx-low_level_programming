cc302006d9bb2ff808ecdc698a0f1af5a9c445bd
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-22 15:23:00 -0700

    add function 1 file

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change value of parameter to certain value
+ * @n: parameter to change
+ *
+ * Return: void so nothin
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
