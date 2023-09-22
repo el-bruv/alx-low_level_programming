34eab1eaa669460b03480cbb34b6e192255e22e3
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-22 16:18:56 -0700

    make temp var non pointer

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function to swap the values of two integers
+ * @a: first integer
+ * @b: second integer
+ *i
+ * Returin: void so nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int h;
+
+	h = *b;
+	*b = *a;
+	*a = h;
+}
