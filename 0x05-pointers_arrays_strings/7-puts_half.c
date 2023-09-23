a3deb8ab4b443d83a37e4ccbb06862ddd9f629d2
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-23 15:34:21 -0700

    add function 7 and header file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - print out the second half of a string
+ * @str: string parameter
+ *
+ * Return: void so nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	if (i % 2 == 0)
+		i = i / 2;
+	else
+		i = (i - 1) / 2;
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
