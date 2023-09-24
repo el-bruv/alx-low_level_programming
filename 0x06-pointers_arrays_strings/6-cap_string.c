cd2937e525a1572c86d458b25592e522570b3356
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-24 16:23:34 -0700

    add function 6 and header file

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * cap_string - function to capitalize all words of a string
+ * @str: string parameter
+ *
+ * Return: pointer to altered string
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			i++;
+		if (str[i] >= '0' && str[i] <= '9')
+			i++;
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i - 1] == ' ' || str[i - 1] == '\t')
+				str[i] -= 32;
+			if (str[i - 1] == '\n' || str[i - 1] == ',')
+				str[i] -= 32;
+			if (str[i - 1] == ';' || str[i - 1] == '.')
+				str[i] -= 32;
+			if (str[i - 1] == '!' || str[i - 1] == '?')
+				str[i] -= 32;
+			if (str[i - 1] == '"')
+				str[i] -= 32;
+			if (str[i - 1] == '(' || str[i - 1] == ')')
+				str[i] -= 32;
+			if (str[i - 1] == '{' || str[i - 1] == '}')
+				str[i] -= 32;
+
+		}
+		i++;
+	}
+	return (str);
+}
