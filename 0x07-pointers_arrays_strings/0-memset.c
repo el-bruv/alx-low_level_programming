bb0e1bda410fa72520e4a508604606e0637c5a60
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-25 16:01:56 -0700

    add function 0, header and readme file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memset - fill memory, n times, with a constant byte
+ * @s: memory area parameter
+ * @b: constant byte
+ * @n: n times parameter
+ *
+ * Return: pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Directory containing C programming projects about points, arrays and strings. Focus being memory manipulation.
