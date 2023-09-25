f4b7f379120602107c034d692b27e1547c1bffd6
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-24 19:20:59 -0700

    add function 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,11 +10,11 @@ int main(void)
 	int i;
 
 	i = 1;
-	while (i <=100)
+	while (i <= 100)
 	{
 		if (i % 3 == 0)
 			printf("Fizz");
-		else if (i % 5 ==0)
+		else if (i % 5 == 0)
 			printf("Buzz");
 		else if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
