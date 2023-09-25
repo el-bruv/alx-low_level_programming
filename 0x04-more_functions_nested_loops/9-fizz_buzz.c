ac03e77314d456626ca8fa6d8baca5e6140220a9
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-24 19:19:36 -0700

    add function 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program to print integers and different values at certain multiples
+ *
+ * Return: zero for success
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <=100)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 ==0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		printf(" ");
+		i++;
+	}
+}
