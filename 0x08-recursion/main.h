bac2f3aef8403c57cfc934ee748777e94bf37dba
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-09-27 10:11:12 -0700

    add function 0 and header file

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main.h - header file of prototype declarations
+ *
+ */
+
+int	_putchar(char c);
+void	_puts_recursion(char *s);
+
+
+
+#endif /* MAIN_H */
