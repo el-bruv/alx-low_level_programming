6992e9f464e6a5ab2bdb2b7329c3716cce073ca3
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-11 19:20:40 -0700

    add function 2 and header file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,29 @@
 #include "main.h"
 
+
+/**
+ * _strlen - calculate string length
+ * @st: string parameter
+ *
+ * Return: length of string
+ */
+int _strlen(char *st)
+{
+	int i;
+
+	if (st == 0)
+	{
+		i = 0;
+	}
+	else
+	{
+		i = 0;
+		while (st[i])
+			i++;
+	}
+	return (i);
+}
+
 /**
  * str_concat - concatenate s2 to s1
  * @s1: first string parameter
@@ -14,37 +38,39 @@ char *str_concat(char *s1, char *s2)
 	int k;
 	char *s;
 
-	if (!s1 || !s2)
-		return (0);
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	i1 = 0;
-	while (s1[i1] != '\0')
-		i1++;
-	i2 = 0;
-	while (s2[i2] != '\0')
-		i2++;
-	i2++;
+	i1 = _strlen(s1);
+	i2 = _strlen(s2);
 	k = i1 + i2;
-	if (k <= 0)
-		return (0);
-	s = malloc(sizeof(char) * k);
+	if (k == 0)
+	{
+		s = malloc(0);
+		return (s);
+	}
+	s = malloc(sizeof(char) * (k + 1));
 	if (!s)
 		return (0);
-	k = 0;
-	while (s1[k] != '\0' && k < i1)
+	if (s1 == 0)
+		i1 = 0;
+	else
 	{
-		s[k] = s1[k];
-		k++;
+		k = 0;
+		while (s1[k] != '\0' && k < i1)
+		{
+			s[k] = s1[k];
+			k++;
+		}
 	}
-	k = 0;
-	while (s2[k] != '\0' && k < i2)
+	if (s2 == 0)
+		i2 = 0;
+	else
 	{
-		s[i1 + k] = s2[k];
-		k++;
+		k = 0;
+		while (s2[k] && k < i2)
+		{
+			s[i1 + k] = s2[k];
+			k++;
+		}
 	}
-	s[i1 + k] = '\0';
+	s[i1 + i2 + 1] = '\0';
 	return (s);
 }
