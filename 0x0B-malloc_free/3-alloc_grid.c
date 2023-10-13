031cba20a6873bdc1cdf379217d1fc0ae296ee83
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-13 10:49:02 -0700

    add function 3 file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,12 +15,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (0);
-	garray = malloc(sizeof **garray * width);
+	garray = malloc(sizeof(**garray) * width);
 	if (garray == NULL)
 		return (0);
 	for (i = 0; i < width; i++)
 	{
-		garray[i] = malloc(sizeof *garray * height);
+		garray[i] = malloc(sizeof(*garray) * height);
 		if (garray[i] == NULL)
 			return (0);
 		for (k = 0; k < height; k++)
