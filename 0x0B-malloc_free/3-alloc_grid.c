125a8496f530bfc988bfd409cc70ecf86ede1666
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-13 11:03:44 -0700

    change malloc sizeof reference

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,12 +15,12 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (0);
-	garray = malloc(sizeof(*garray) * width);
+	garray = (int **)malloc(sizeof(*garray) * width);
 	if (garray == NULL)
 		return (0);
 	for (i = 0; i < width; i++)
 	{
-		garray[i] = malloc(sizeof(garray) * height);
+		garray[i] = (int *)malloc(sizeof(garray) * height);
 		if (garray[i] == NULL)
 			return (0);
 		for (k = 0; k < height; k++)
