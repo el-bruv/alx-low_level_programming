674e10aeaf25db1b3d2e284f2a4a4f6849b47964
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-15 01:56:26 -0700

    add funciton 0 file

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -11,8 +11,8 @@ void *malloc_checked(unsigned int b)
 	void *ptr;
 
 	if (b <= 0)
-		return (0);
-	if (b % 4 != 0)
+		exit(98);
+	if (b % 2 != 0)
 		exit(98);
 	ptr = malloc(b);
 	if (ptr == NULL || ptr == 0)
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/**
+ * Description: header file containing protoypes for memory allocation and
+ * manipulation
+ *
+ */
+int	_putchar(char c);
+void	*malloc_checked(unsigned int b);
+
+#endif /* MAIN_H */
