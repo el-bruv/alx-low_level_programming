4dc3210e275e7e1b01b0a3998cc0ad46223efeab
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-15 14:26:59 -0700

    add task 0 file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - to hold basic info provided about a dog
+ * @name: name of dog parameter
+ * @age: age of dog parameter
+ * @owner: owner of dog parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
