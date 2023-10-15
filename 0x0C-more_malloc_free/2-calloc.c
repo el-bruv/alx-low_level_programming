7d733cf3c97a74937fb23f00726e5ebfffec4f8a
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-15 04:01:17 -0700

    add function 2 and header file

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _calloc - allocate array memory of # of elements, each being size of size
+ * @nmemb: number of elements parameter
+ * @size: size of each element parameter
+ *
+ * Return: pointer to memory allocation, NULL for failure
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(sizeof(size) * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
