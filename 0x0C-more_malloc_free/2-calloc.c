19cf32a989039e062fc55bc1f10b0509b01b28cd
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-15 04:26:37 -0700

    add function 2 file

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,15 +9,12 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
-	unsigned int i;
+	void *ptr;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	ptr = malloc(size * nmemb);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
 	return (ptr);
 }
