419585a6e6322589a72fbf0eb9b49265aa5c11cb
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-16 04:26:22 -0700

    add header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef	struct dog dog_t;
+
 /**
  * struct dog - to hold basic info provided about a dog
  * @name: name of dog parameter
@@ -16,6 +22,6 @@ struct dog
 
 void	init_dog(struct dog *d, char *name, float age, char *owner);
 void	print_dog(struct dog *d);
-typedef	struct dog dog_t;
+dog_t	*new_dog(char *name, float age, char *owner);
 
 #endif /* DOG_H */
