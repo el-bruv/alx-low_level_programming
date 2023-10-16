a4cf5e9b35c9bce10d407ec7a8d5ff5b9f5fcb41
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-16 00:21:45 -0700

    add task 2 file

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,6 +2,25 @@
 #include <stdio.h>
 
 /**
+ * _sl - determine the lenght of a string
+ * @s: string parameter
+ *
+ * Return: length of string
+ */
+int _sl(char *s)
+{
+	int i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+
+/**
  * print_dog - print out the contents of a struct
  * @d: stuct parameter
  *
@@ -11,10 +30,16 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
-	if (!(d->age))
+	if (d->name == NULL || _sl(d->name) == 0)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age < 0.0f)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %f\n", d->age);
-	printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	if (d->owner == NULL || _sl(d->name) == 0)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
