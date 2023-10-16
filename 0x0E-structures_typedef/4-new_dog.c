a1bbb55f065229708833ee39c39cd3794c68723d
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-16 07:11:01 -0700

    add task 4 file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,8 +14,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *doggo;
 
 	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
+		return (NULL);
 	doggo->name = name;
+	if (doggo->name != name)
+		return (NULL);
 	doggo->age = age;
+	if (doggo->age != age)
+		return (NULL);
 	doggo->owner = owner;
+	if (doggo->owner != owner)
+		return (NULL);
 	return (doggo);
 }
