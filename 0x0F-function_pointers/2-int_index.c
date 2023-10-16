a6cc192ecb19296278ab152c75a0690a0a268868
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-16 12:50:00 -0700

    add task 2 and header file

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - search for an integer, using a function pointer and return its \
+ * index
+ * @array: array of integers parameter
+ * @size: length of array parameter
+ * @cmp: pointer to function parameter
+ *
+ * Return: index of identified integer, -1 for no match or incorrect size
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return (0);
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if ((*cmp)(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
