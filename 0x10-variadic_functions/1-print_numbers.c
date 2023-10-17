ddcaefa2113d0bb9d7a4e25103030a6bf8b17174
Mulunghisi Nukeri <nukeri800@gmail.com>
2023-10-16 18:49:45 -0700

    add task 1 and header file

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - function to print out  argument parameters
+ * @separator: string to print between numbers
+ * @n: counter parameter
+ *
+ * Return: void so nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list pl;
+
+	if (n == 0 || separator == NULL)
+		return;
+	va_start(pl, n);
+	i = 0;
+	printf("%d", va_arg(pl, int));
+	i++;
+	while (i < n)
+	{
+		printf("%s%d", separator, va_arg(pl, int));
+		i++;
+	}
+	va_end(pl);
+	printf("\n");
+}
